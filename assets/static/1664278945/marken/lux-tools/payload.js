0843e7c23306a2efdbfdacb7d4f486b7ecc4d577
outdoorhosen24-de <outdoorhosen24-de@users.noreply.github.com>
2022-09-27 11:42:55 +0000

    deploy: 28ada69b1ebe66bca4aed9436664d70564da99cd

---
+++ assets/static/1664278945/marken/lux-tools/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/marken/lux-tools", {data:[{_img:{"/_ipx/f_webp,q_90,s_407x229/assets/images/images/lux-fahrradhalter-30-kg-lux-tools-4147906-1.jpg":"\u002Fassets\u002Fimages\u002Flux-fahrradhalter-30-kg-lux-tools-4147906-1.webp"}}],fetch:{},mutations:void 0});
